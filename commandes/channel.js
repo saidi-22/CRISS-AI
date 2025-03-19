8268422fe0b4972e294491c5ada9676c5ad513d3
CRISS VEVO <crissvevo@icloud.com>
2025-03-20 01:55:26 +0300

    Add files via upload

--- commandes/channel.js
+++ commandes/channel.js
@@ -1,3 +1,4 @@
+
 const util = require('util');
 const fs = require('fs-extra');
 const { zokou } = require(__dirname + "/../framework/zokou");
@@ -8,7 +9,7 @@ const s = require(__dirname + "/../set");
 const more = String.fromCharCode(8206)
 const readmore = more.repeat(4001)
 
-zokou({ nomCom: "main", categorie: "All Groups" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "web", categorie: "General" }, async (dest, zk, commandeOptions) => {
     let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
     let { cm } = require(__dirname + "/../framework//zokou");
     var coms = {};
@@ -33,21 +34,101 @@ zokou({ nomCom: "main", categorie: "All Groups" }, async (dest, zk, commandeOpti
 const temps = moment().format('HH:mm:ss');
 const date = moment().format('DD/MM/YYYY');
 
-  let infoMsg =      `𝗙𝗢𝗟𝗟𝗢𝗪 𝗠𝗬 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝗙𝗢𝗥 𝗙𝗥𝗘𝗘 𝗕𝗢𝗧* 
-𝗙𝗢𝗟𝗟𝗢𝗪 𝗠𝗬 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝗙𝗢𝗥 𝗙𝗥𝗘𝗘 𝗕𝗢𝗧* 
-╔══════════════════╗
-║𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝐋𝐈𝐍𝐊 𝐇𝐄𝐑𝐄
- >https://shorturl.at/10YNe
-╚══════════════════╝
-╔══════════════════╗
-║☉︎𝐖𝐏-𝐌𝐄-𝐎𝐖𝐍𝐄𝐑®️
- >https://shorturl.at/H5hVK
-╚══════════════════╝
+  let infoMsg =  `
+╭───────────────━⊷
+┊ *VISIT OUR WEBSITE*
+┊ https://criss-md-3.onrender.com
+╰───────────────━⊷
+ `;    
+let menuMsg = `
+`;
 
-> 𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 𝗖𝗥𝗜𝗦𝗦 𝗩𝗘𝗩𝗢
-\n
+   var lien = mybotpic();
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🤫🤫 Menu erreur " + e);
+        repondre("🤫🤫 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🤫🤫 Menu erreur " + e);
+        repondre("🤫🤫 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
+}
+
+}); 
+
+
+/*const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
+
+zokou({ nomCom: "script", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//zokou");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+
+
+    
+
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('Etc/GMT');
+
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+  let infoMsg =  `
+   *BMW MD IMPORTANT INFO* 
+❒───────────────────❒
+*GITHUB LINK*
+> https://github.com/ibrahimaitech/BMW-MD
+
+*WHATSAPP CHANNEL*
+> https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y
+
+*FOR MORE INFO TAP ON THE LINK BELOW*
+> https://github.com/IBRAHIM-TECH-AI/IBRAHIM-ADAMS-INFO⁠
+╭───────────────────❒
+│❒⁠⁠⁠⁠ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+│❒⁠⁠⁠⁠ *DEV* : *Ibrahim Adams*
+⁠⁠⁠⁠╰───────────────────❒
   `;
-    let menuMsg = ``;
+    
+let menuMsg = `
+     𝑰𝑩𝑹𝑨𝑯𝑰𝑴 𝑨𝑫𝑨𝑴𝑺 𝑺𝑪𝑰𝑬𝑵𝑪𝑬
+
+❒────────────────────❒`;
 
    var lien = mybotpic();
 
@@ -76,4 +157,4 @@ else {
     
 }
 
-}); 
+});*/
