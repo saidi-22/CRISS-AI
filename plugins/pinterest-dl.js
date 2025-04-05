797fb423d72202fee458541fb28d0031e44f75aa
CRISS VEVO <crissvevo@icloud.com>
2025-04-05 13:50:34 +0300

    Update pinterest-dl.js

--- plugins/pinterest-dl.js
+++ plugins/pinterest-dl.js
@@ -32,7 +32,7 @@ cmd({
         const videoUrl = media.find(item => item.type.includes('720p'))?.download_url || media[0].download_url;
 
         // Prepare the new message with the updated caption
-        const desc = `╭━━━〔 *KHAN-MD* 〕━━━┈⊷
+        const desc = `╭━━━〔 *CRISS-AI* 〕━━━┈⊷
 ┃▸╭───────────
 ┃▸┃๏ *PINS DOWNLOADER*
 ┃▸└───────────···๏
@@ -41,7 +41,7 @@ cmd({
 ┇๏ *Title* - ${title}
 ┇๏ *Media Type* - ${media[0].type}
 ╰━━❑━⪼
-> *© Pᴏᴡᴇʀᴇᴅ Bʏ KʜᴀɴX-Aɪ ♡*`;
+ *©𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 𝗖𝗥𝗜𝗦𝗦 𝗩𝗘𝗩𝗢*`;
 
         // Send the media (video or image) to the user
         if (videoUrl) {
