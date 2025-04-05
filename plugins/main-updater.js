814a0605668481b992073ba1a23ac908d5528025
CRISS VEVO <crissvevo@icloud.com>
2025-04-05 10:40:10 +0300

    Update main-updater.js

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -16,24 +16,24 @@ cmd({
     if (!isOwner) return reply("This command is only for the bot owner.");
 
     try {
-        await reply("🔍 Checking for KHAN-MD updates...");
+        await reply("🔍 Checking for CRISS-AI updates...");
 
         // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://api.github.com/repos/XdTechPro/KHAN-MD/commits/main");
+        const { data: commitData } = await axios.get("https://api.github.com/repos/Xcriss-vevo/CRISS-AI/commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
         const currentHash = await getCommitHash();
 
         if (latestCommitHash === currentHash) {
-            return reply("✅ Your KHAN-MD bot is already up-to-date!");
+            return reply("✅ Your CRISS-AI bot is already up-to-date!");
         }
 
-        await reply("🚀 Updating KHAN-MD Bot...");
+        await reply("🚀 Updating CRISS-AI Bot...");
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/XdTechPro/KHAN-MD/archive/main.zip", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/criss-vevo/CRISS-AI/archive/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
@@ -44,7 +44,7 @@ cmd({
 
         // Copy updated files, preserving config.js and app.json
         await reply("🔄 Replacing files...");
-        const sourcePath = path.join(extractPath, "KHAN-MD-main");
+        const sourcePath = path.join(extractPath, "CRISS-AI-main");
         const destinationPath = path.join(__dirname, '..');
         copyFolderSync(sourcePath, destinationPath);
 
