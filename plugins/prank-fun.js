9e32c84ebaaeae2b283174c182c6279f47d094fa
CRISS VEVO <crissvevo@icloud.com>
2025-04-05 13:54:34 +0300

    Update prank-fun.js

--- plugins/prank-fun.js
+++ plugins/prank-fun.js
@@ -43,7 +43,7 @@ async (conn, mek, m, {
             '⚠️ *Note:* All actions are for demonstration purposes only.',
             '⚠️ *Reminder:* Ethical hacking is the only way to ensure security.',
             
-            '> *KHAN-MD-HACKING-COMPLETE ☣*'
+            '> *CRISS-AI-HACKING-COMPLETE ☣*'
         ];
 
         for (const line of steps) {
